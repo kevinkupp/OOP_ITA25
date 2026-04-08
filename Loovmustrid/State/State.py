32e921edaf89d1d588dc31917ff2457a12ddd873
kevin.kupp <kevin.kupp@voco.ee>
2026-04-08 12:04:24 +0300

    State

---
+++ Loovmustrid/State/State.py
@@ -0,0 +1,37 @@
+class CombinationLock:
+    def __init__(self, combination):
+        self.combinations = combination
+        self.status = "LOCKED"
+        self.entered_digits = []
+
+    def enter_digit(self, digit):
+        self.entered_digits.append(digit)
+        current_step = len(self.entered_digits)
+        correct_part = self.combinations[:current_step]
+
+        if self.entered_digits == correct_part:
+            if len(self.entered_digits) == len(self.combinations):
+                self.status = "OPEN"
+            else:
+                self.status = "".join(map(str, self.entered_digits))
+        else:
+            self.status = "ERROR"
+
+if __name__ == "__main__":
+    cl = CombinationLock([1, 2, 3, 4, 5])
+    print(cl.status)
+
+    cl.enter_digit(1)
+    print(cl.status)
+
+    cl.enter_digit(2)
+    print(cl.status)
+
+    cl.enter_digit(3)
+    print(cl.status)
+
+    cl.enter_digit(4)
+    print(cl.status)
+
+    cl.enter_digit(5)
+    print(cl.status)
